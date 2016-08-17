5cdff8576571c6d2bfb5a781c840d1da8835c6eb
Patrick R <patrick.rein@gmail.com>
2016-08-17 17:51:00 +0200

    Sorts the list of days

--- done-tasks.rb
+++ done-tasks.rb
@@ -37,6 +37,7 @@ get '/' do
     tasks_per_week = tasks_per_week.select do |day, t|
       Date.today - day < 7
     end
+    tasks_per_week = tasks_per_week.sort
 
     erb :index, :locals => { :tasks_per_week => tasks_per_week, :user_name => user.name }
   end
