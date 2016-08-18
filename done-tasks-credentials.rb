18c133cfda7b9ed9c1be28fe24fdcd61d2e32618
Patrick R <patrick.rein@gmail.com>
2016-08-18 09:31:24 +0200

    Removes the test credentials from the module

--- done-tasks-credentials.rb
+++ done-tasks-credentials.rb
@@ -1,5 +1,5 @@
 module Credentials
-  CLIENT_ID = "a9bcc58b3bf5dc9796b2"
-  CLIENT_SECRET = "7bd2f246e5eb67b9d18653dec325c4b669e28842a5106abf1a87da1bab5e"
-  REDIRECT_URL = "http://done-tasks.herokuapp.com/authorize"
+  CLIENT_ID = ""
+  CLIENT_SECRET = ""
+  REDIRECT_URL = ""
 end
